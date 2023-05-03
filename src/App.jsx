7cee98c4da6d3ab1d0ab64a1ec70e494d6909b57
ClecioSousa00 <cleciosouza015@gmail.com>
2023-05-03 09:03:52 -0300

    refactoring component InputSearch

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 
 import { useState } from 'react';
-// import axios from 'axios';
 
 import { Container } from './components/Container'
 import { InputSearch } from './components/InputSearch'
 import { ResultCep } from './components/ResultCep'
 import { Loader } from './components/Loader';
+
 import { axiosInstance } from './helper/axios-instance';
 
 export const App = () => {
@@ -13,26 +13,22 @@ export const App = () => {
   const [cep, setCep] = useState()
   const [loading, setLoading] = useState(false)
 
-  const handleSearch = async (input) => {
+  const handleSearch = async (input, event) => {
+    event.preventDefault()
     setLoading(true)
     try {
       const response = await axiosInstance.get(`${input}/json`)
-      // const response = await axios.get(`https://viacep.com.br/ws/${input}/json/`)
       setCep(response.data)
     } catch {
       setCep('O CEP informado não foi encontrado')
     }
-    // setLoading(false)
     setTimeout(() => {setLoading(false)},400)
   }
-
 
   return (
     <>
       <Container>
         <InputSearch handleSearch={handleSearch} />
-        {/* {loading && <Loader/>}
-        {cep && <ResultCep cep={cep} />} */}
         {loading ? <Loader/> : cep && <ResultCep cep={cep} />}
       </Container>
     </>
--- src/components/InputSearch/index.jsx
+++ src/components/InputSearch/index.jsx
@@ -2,20 +2,18 @@ import { useState } from 'react';
 import './InputSearch.css'
 import { FaSearch } from "react-icons/fa";
 
-
 export const InputSearch = ({handleSearch}) =>{
 
     const [input, setInput] = useState('')
 
-    
     return(
-        <div className="container_input">
-            <input value={input} 
+        <form onSubmit={(event) =>handleSearch(input,event)} className="container_input">
+            <input 
                 type="text" 
                 placeholder="Digite seu CEP"
                 onChange={event => setInput(event.target.value)}
             />
-            <button onClick={() => handleSearch(input)}  className="button_search"> <FaSearch/> </button>
-        </div>
-    )
+            <button type='submit'   className="button_search"> <FaSearch/> </button>
+        </form>
+    )  
 }
