cd8a275f80a108b0c7e571b576fcf8ad491b8957
ClecioSousa00 <cleciosouza015@gmail.com>
2023-05-12 19:12:11 -0300

    validating zip code with regex

--- src/App.jsx
+++ src/App.jsx
@@ -12,15 +12,28 @@ export const App = () => {
 
   const [cep, setCep] = useState()
   const [loading, setLoading] = useState(false)
+  const cepRegex = /^[0-9]{5}-?[0-9]{3}$/;
 
   const handleSearch = async (input, event) => {
     event.preventDefault()
     setLoading(true)
     try {
+
+      if(!cepRegex.test(input)){
+        throw new Error('O CEP informado é inválido. Certifique que ele tenha 8 caracteres de 0 a 9')
+      }
+
       const response = await axiosInstance.get(`${input}/json`)
+      
+
+      if(response.data.erro){
+        throw new Error('Não foi possível obter os dados')
+      }
+      
       setCep(response.data)
-    } catch {
-      setCep('O CEP informado não foi encontrado')
+
+    } catch (err){
+      setCep(err.message)
     }
     setTimeout(() => {setLoading(false)},400)
   }
